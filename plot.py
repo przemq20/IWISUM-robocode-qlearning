f8ba9ab225dbf6191fbfd24359ad84517753c3c2
przemq20 <lechowiczprzemyslaw@gmail.com>
2023-01-01 16:10:45 +0100

    Running average

--- plot.py
+++ plot.py
@@ -6,7 +6,8 @@ columns = ["round", "reward"]
 
 df = pd.read_csv("out/production/RobocodeQlearning/QLearner.data/data.csv",usecols = columns)
 
+running = df['reward'].rolling(10).mean()
 plt.xlabel('Round')
 plt.ylabel('Reward')
-plt.plot(df['round'], df['reward'])
+plt.plot(df['round'], df['reward'], running)
 plt.show()
